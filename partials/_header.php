219e5b6e6480b0c0d7eb133adfa8469188fad123
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-02-12 19:35:15 +0545

    Creating Login Handler & Header for Logged in User

--- partials/_header.php
+++ partials/_header.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 
 echo '  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
@@ -6,39 +7,54 @@ echo '  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/forum">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="about.php">About</a>
-                </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Categories
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.php">Contact</a>
-                </li>
-            </ul>
-          
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-success" type="submit">Search</button>
-            </form>
-              <div class="mx-2"> 
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#signupModal">Signup</button>
-            </div>
-            </div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/forum">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Categories
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                </ul>';
+     
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
+    echo '<form class="d-flex align-items-center" role="search"> 
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> 
+        <button class="btn btn-success me-3" type="submit">Search</button> 
+        <span class="text-white ms-2 text-nowrap me-2">Welcome '. $_SESSION['useremail'] .'</span> 
+        <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Logout</button>
+    </form>';
+    
+
+}               
+else{
+    echo '<form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-success" type="submit">Search</button>
+                     Welcome Manish
+                </form>
+                <div class="mx-2"> 
+                    <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+                    <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#signupModal">Signup</button>
+                </div>';
+}
+               
+    echo ' </div>
         </div>   
     </nav>';
  
